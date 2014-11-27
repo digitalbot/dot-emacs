0b3cf5a98ef58385834d7e284cbbb902643f6e90
k7-nakamura <k7-nakamura@nri.co.jp>
2014-11-27 18:05:55 +0900

    add malabar-mode in windows

---
+++ conf/30_java.el
@@ -0,0 +1,23 @@
+;;;----------------------------------------
+;;; java
+;;;----------------------------------------
+
+(require 'cedet)
+(setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
+                                  global-semanticdb-minor-mode
+                                  global-semantic-idle-summary-mode
+                                  global-semantic-mru-bookmark-mode))
+
+(semantic-mode 1)
+(require 'malabar-mode)
+(add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+
+(when windows-p
+  (setq malabar-util-path-separator ";")
+  (setq malabar-util-groovy-file-filter 'malabar-util-reverse-slash))
+
+(require 'ajc-java-complete-config)
+(add-hook 'malabar-mode-hook 'ajc-java-complete-mode)
+(add-hook 'java-mode-hook 'ajc-java-complete-mode)
+
+
