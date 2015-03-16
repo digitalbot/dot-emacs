73f236ddddd340436812bed06fd9428e6261cbc8
k7-nakamura <k7-nakamura@nri.co.jp>
2015-03-16 09:57:35 +0900

    fix

--- conf/40_eshell.el
+++ conf/40_eshell.el
@@ -4,6 +4,7 @@
 
 (require 'eshell)
 (defun eshell/clear ()
+  (interactive)
   (let ((inhibit-read-only t))
     (erase-buffer)))
 
@@ -23,12 +24,12 @@
 
 (add-hook 'eshell-mode-hook
           (lambda ()
-            (my-ac-eshell-mode)
-            (helm-mode nil)
-            (auto-complete-mode 1)
             (smartparens-mode 1)
-            (define-key eshell-mode-map (kbd "C-,") 'helm-eshell-history)
-            
+            (my-ac-eshell-mode)
+            (auto-complete-mode 1)
+            (setq ac-auto-start nil)
+            (local-set-key (kbd "C-,") 'helm-eshell-history)
+            (local-set-key (kbd "C-q C-l") 'ehsell/clear)
             ;; (define-key eshell-mode-map (kbd "M-p") 'helm-eshell-history)
             ;; (define-key eshell-mode-map (kbd "M-n") 'helm-esh-pcomplete)
             ;; (define-key eshell-mode-map (kbd "C-a") 'eshell-bol)
