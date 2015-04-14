03c45b6acd1569c9bb780b48923b7c484db9b91f
k7-nakamura <k7-nakamura@nri.co.jp>
2015-04-14 13:42:53 +0900

    dired-filter hook

--- conf/20_dired.el
+++ conf/20_dired.el
@@ -10,3 +10,9 @@
 (setq dired-isearch-filenames t)
 (define-key dired-mode-map (kbd "(") 'dired-hide-details-mode)
 (define-key dired-mode-map (kbd ")") 'dired-hide-details-mode)
+
+;; dired-filter
+(defun my-dired-mode-hooks()
+  (dired-filter-mode))
+
+(add-hook 'dired-mode-hook 'my-dired-mode-hooks)
